428065cf0e0e8c6bd727cca34ce6eba881543def
Hernan Matiello <hernan.matiello@NAVE035-DVDHILK.nave.local>
2016-06-24 11:29:55 -0300

    Primos2003_Dayvid_Gustavo_Hernan
    Trabalho dos primos 2003

---
+++ GenePrime/ConsoleApplication1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Primos p = new Primos();
+
+            Console.WriteLine("Até que numero você deseja?");
+            p.limit = int.Parse(Console.ReadLine());
+
+            p.initNumbers(p.limit);
+
+            p.markAndDelete();
+
+            p.numbersAsString();
+        }
+    }
+}
---
+++ GenePrime/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Primos
+    {
+        public int limit = 20000;
+        public List<int> numbers = new List<int>();
+
+        public int currentPosition = -1;
+
+        public void initNumbers(int limit)
+        {
+            for (int i = 2; i <= limit; i++)
+            {
+                //Console.WriteLine(i.ToString());
+                numbers.Add(i);
+            }
+
+        }
+
+        public void markAndDelete()
+        {
+            currentPosition++;
+            if (numbers.Count > currentPosition)
+            {
+                int currentValue = numbers.ElementAt(currentPosition);
+                for (int i = currentPosition + 1; i < numbers.Count; i++)
+                {
+                    if (numbers.ElementAt(i) % currentValue == 0) // não é primo
+                    {
+                        numbers.RemoveAt(i);
+                    }
+                }
+                markAndDelete();
+            }
+        }
+        public void numbersAsString()
+        {
+            foreach (var item in numbers)
+            {
+                Console.Write(item.ToString() + "; ");
+            }
+            Espera();
+        }
+
+        public void Espera()
+        {
+            Console.ReadKey();
+        }
+
+    }
+}
